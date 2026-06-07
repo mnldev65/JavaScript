0e6c80bd60903dd370086ceeee1aaa9f74d3e509
Marlon Nunes <mnlmarlon936@gmail.com>
2026-06-07 11:43:51 -0300

    if and else no js

--- Projetos_práticos/DevSorteio/script.js
+++ Projetos_práticos/DevSorteio/script.js
@@ -2,7 +2,10 @@ function generateNumber() {
     const num01 = Math.ceil(document.querySelector('.input-value').value)
     const num02 = Math.floor(document.querySelector('.input-value-dois').value)
     
-    const result = Math.floor(Math.random() * (num02 - num01 + 1)) + num01
-    
-    alert(result)
+    if (num02 > num01) {
+        const result = Math.floor(Math.random() * (num02 - num01 + 1)) + num01
+        alert(result)
+    } else {
+        alert('O valor minimo tem que ser menor que o máximo')
+    }
 }
