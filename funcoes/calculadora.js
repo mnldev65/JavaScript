0e8e72f87114db33e07a7e359fc983c6fb995728
Marlon Nunes <mnlmarlon936@gmail.com>
2026-09-06 16:04:41 -0300

    teste calcular função

---
+++ funcoes/calculadora.js
@@ -0,0 +1,32 @@
+function adicao(value1, value2) {
+    console.log(value1 + value2)
+}
+
+adicao(5,10)
+adicao(5,0)
+adicao(5,100)
+
+function subtracao(value1, value2) {
+    console.log(value1 - value2)
+}
+
+subtracao(10,5)
+subtracao(10,0)
+subtracao(10,100)
+
+function multiplicacao(value1, value2) {
+    console.log(value1 * value2)
+}
+
+multiplicacao(5,10)
+multiplicacao(5,0)
+multiplicacao(5,100)
+
+function divisao(value1, value2) {
+    console.log(value1 / value2)
+}
+
+divisao(10,5)
+divisao(10,0)
+divisao(10,100)
+
